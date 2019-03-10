d7819f54a5b61f0241623a53d1e17a87b0f33a97
Anatoly Kulikov <dev@anatolykulikov.ru>
2019-03-10 10:46:09 +0300

    lesson_2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,110 @@
-const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
-  
-const renderGoodsItem = (title, price) => 
-    `<div class="goods-item">
-      <h3>${title}</h3>
-      <p>${price}</p>
-    </div>`;
-  
-const renderGoodsList = list => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-    // Если в метод join в качестве необязательного параметра разделителя мы указываем любой символ (либо не указываем вообще), то между элементами будет выводится символ/запятая (по умолчанию). В данном случае сепаратор указан пустым, ничего не выводится.
+class GoodItem {
+    constructor(title, price, img) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goods-item">
+                <div class="goods-info">
+                  <img src="${this.img || 'img/no-image.jpg'}" alt="${this.title}">
+                  <h3>${this.title}</h3>
+                  <p>${this.price || 'Цена по запросу'}</p>
+                </div>
+                <button class='addClick'>Добавить</button>
+              </div>`;
+    }
 }
-  
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [{
+                title: 'Shirt',
+                price: 150,
+                img: "images/shirt.jpg"
+            },
+            {
+                title: 'Socks',
+                price: 50,
+                img: 'images/socks.jpg'
+            },
+            {
+                title: 'Jacket',
+                price: 350,
+                img: 'images/jacket.jpg'
+            },
+            {
+                title: 'Shoes',
+                price: 250,
+                img: 'images/shoes.jpg'
+            }
+        ]
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach((good) => {
+            const goodItem = new GoodItem(good.title, good.price, good.img);
+            listHtml += goodItem.render();
+        })
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    calcAllGoods() {
+        let totalPrice = 0;
+        this.goods.forEach((good) => {
+            totalPrice += good.price;
+        });
+        let totalGoodsAnswer = "Все товары на сумму $" + totalPrice;
+        document.querySelector('.goods-total').innerHTML = totalGoodsAnswer;
+    }
+}
+
+// Класс элемента корзины
+class BasketItem {
+    // По сути, нам нужно отображать в корзине те же самые элементы, что и в списке
+    constructor(title, price, img, link) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+        this.link = link; // Вероятно, ссылка на страницу товара
+    }
+    // Однако внешний вид может быть другим (пока не прописывал)
+    render() {
+
+    }
+}
+// Класс корзины
+class Basket {
+    constructor() {
+        // В классе корзины массив с добавленными товарами
+        this.addGoods = [];
+        this.deletedGoods = []; // Можно заморочится и добавить товары, которые были удалены из корзины (их можно быстро вернуть в список - убираем боль/проблему поиска, если удалено случайно/пользователь передумал)
+    }
+    // Добавление товара в корзину (привязываем на нажатие кнопки)
+    addToBasket() {}
+
+    // Удаление товара из корзины (привязываем на нажатие кнопки)
+    deleteFromBasket() {}
+
+    // Считаем стоимость и количество товаров в корзине
+    calcBasket() {}
+
+    // Метод, который определяет, добавлены ли в корзину какие-либо товары и при их наличии активирует кнопку "Оформить заказ"
+    isOrder() {}
+
+    // Рендер динамического содержимого корзины
+    render() {}
+
+    // Метод открывания корзины
+    openBasket() {}
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+
 window.onload = () => {
-    renderGoodsList(goods);
+    list.render();
+    list.calcAllGoods();
 };
-// Честно, без понятия, чего здесь можно сократить. Как вариант, можно создать сжатый js, но пока не знаю, как можно создавать сжатые файлы автоматически (наподобие sass-компиляции). Рядом с основным есть сжатый js, но сделан с помощью онлайн-инструмента.
